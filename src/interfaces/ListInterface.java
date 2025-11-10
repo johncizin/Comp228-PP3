ebd3082fea2cde8466c17b82455c04dea7f70f51
John Cizin <68138359+SoaringAbove@users.noreply.github.com>
2025-11-10 18:38:15 -0500

    ListInterface modifications

--- src/interfaces/ListInterface.java
+++ src/interfaces/ListInterface.java
@@ -1,13 +1,29 @@
 package interfaces;
 
-public interface QueueInterface<E> {
+public interface ListInterface<E> {
 
-	void enqueue(E element);  // add an element to the queue - always at the end of the queue
-	
-	E dequeue();   // remove and return the front of the queue
-	
-	boolean isEmpty();
-	
-	boolean isFull();
-	
+	  void add(E element);
+	  
+	  boolean remove (E element);
+	  // remove the first item on the list such that item.equals(element) is true
+	  // if no such item exists, return false
+	  
+	  int size();   // return the number of items on this list
+
+	  boolean isEmpty();
+
+	  boolean contains (E element);
+	  // return true if there is an item on the list such that item.equals(element) is true
+	  // otherwise return false
+	    
+	  E get(E element);
+	  // return the first item on the list such that item.equals(element) is true
+	  // if no such item exists, return null
+	  
+	  E get(int index);
+	  // return the item at the specified position on the list;
+	  // the first item is at index equal to zero
+	  // if the index value is out of bounds, return null
+	  
+
 }
